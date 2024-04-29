379d33230c9549ae78756dda3306d077e0b390dc
cdubuque <chancedubuque@gmail.com>
2024-04-29 12:16:03 -0700

    Add config.json to .gitignore

---
+++ printEpisodes.py
@@ -0,0 +1,29 @@
+import requests
+import os
+import json
+
+# import config file
+with open('config.json', 'r') as file:
+    content = file.read()
+    if not content:
+        print("The file is empty")
+    else:
+        config = json.loads(content)
+
+buzzsprout_api_key = config['database']['buzzsprout_api_key']
+podcast_id = config['database']['podcast_id']
+url = "https://www.buzzsprout.com/api/" + podcast_id + "/episodes.json"
+
+headers = {
+    "Authorization": buzzsprout_api_key
+}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    episodes = response.json()
+    # Process the episodes data here
+    for episode in episodes:
+        print(episode)
+else:
+    print("Failed to retrieve episodes. Status code:", response.status_code)
